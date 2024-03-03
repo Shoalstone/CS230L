52d65f0640ff10e4c0594945c6a21417f71faba5
Shoalstone <shoalstone@outlook.com>
2024-03-03 13:16:56 -0500

    bootstrap components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import Card from './Card';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>CS 230L</h1>
       <h2>Section - 01</h2>
       <p>WVU ID: 800336452</p>
       <p>Hi I am James</p>
+      <Card title="Card 1" content="lorem ipsum" />
+      <Card title="Card 2" content="lorem ipsum" />
+      <Card title="Card 3" content="lorem ipsum" />
     </div>
   );
 }
